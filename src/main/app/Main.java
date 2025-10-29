d3a9af8ab91dbeb280aff0f075b37ab0dbf607f0
poorvi-19 <poorvi1902.singh@gmail.com>
2025-10-29 21:12:23 +0530

    Update Main.java

--- src/main/app/Main.java
+++ src/main/app/Main.java
@@ -1,5 +1,14 @@
-package main.app;
+package app;
+
+import app.ui.LoginFrame;
+
+import javax.swing.*;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            LoginFrame lf = new LoginFrame();
+            lf.setVisible(true);
+        });
+    }
 }
